6ab615036138a87177f6c7e47ec8891707925658
Bruno Cabral <cabral.brc@gmail.com>
2024-03-19 10:53:34 -0700

    support for extra mouse buttons

--- src/main.h
+++ src/main.h
@@ -37,7 +37,8 @@
 
 #define GROMIT_MOUSE_EVENTS ( GDK_BUTTON_MOTION_MASK | \
                               GDK_BUTTON_PRESS_MASK | \
-                              GDK_BUTTON_RELEASE_MASK )
+                              GDK_BUTTON_RELEASE_MASK | \
+                              GDK_POINTER_MOTION_MASK )
 
 #define GROMIT_WINDOW_EVENTS ( GROMIT_MOUSE_EVENTS | GDK_EXPOSURE_MASK)
 
@@ -105,6 +106,19 @@ typedef struct
   gdouble         pressure;
 } GromitPaintContext;
 
+
+typedef struct {
+    guint buttons;
+    guint modifiers;
+} GromitState;
+
+
+typedef struct {
+    GromitState state;
+    gchar *name;
+} GromitLookupKey;
+
+
 typedef struct
 {
   gdouble      lastx;
@@ -113,7 +127,7 @@ typedef struct
   GList*       coordlist;
   GdkDevice*   device;
   guint        index;
-  guint        state;
+  GromitState  state;
   GromitPaintContext *cur_context;
   gboolean     is_grabbed;
   gboolean     was_grabbed;
@@ -190,7 +204,7 @@ void show_window (GromitData *data);
 
 void parse_print_help (gpointer key, gpointer value, gpointer user_data);
 
-void select_tool (GromitData *data, GdkDevice *device, GdkDevice *slave_device, guint state);
+void select_tool (GromitData *data, GdkDevice *device, GdkDevice *slave_device, GromitState state);
 
 void copy_surface (cairo_surface_t *dst, cairo_surface_t *src);
 void snap_undo_state(GromitData *data);
@@ -212,5 +226,8 @@ GromitPaintContext *paint_context_new (GromitData *data, GromitPaintType type,
 void paint_context_free (GromitPaintContext *context);
 
 void indicate_active(GromitData *data, gboolean YESNO);
+
+gboolean compare_state(GromitState lhs, GromitState rhs);
+gchar *key2string(GromitLookupKey key);
 
 #endif
